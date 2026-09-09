da15ccdf36e943d0549a4d150d1d782bfb30065c
aloosh977 <alisuleiman977@gmail.com>
2026-09-09 17:55:02 +0300

    removed unnecessary fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
 	import { onMount } from 'svelte';
 	import '../app.css';
-	import '@fontsource/archivo-black';
-	import '@fontsource/inter/400.css';
-	import '@fontsource/inter/500.css';
-	import '@fontsource/inter/600.css';
-	import '@fontsource/inter/700.css';
+	import '@fontsource/archivo-black/latin.css';
+	import '@fontsource/inter/latin-400.css';
+	import '@fontsource/inter/latin-600.css';
+	import '@fontsource/inter/latin-700.css';
 	import Nav from '$lib/components/Nav.svelte';
 	import Toast from '$lib/components/Toast.svelte';
 	import { ensureSeed } from '$lib/seed.js';
